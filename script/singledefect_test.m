e32cee4a8aee3604176b9e0228d24842707bbb53
Plswearpants <94640411+Plswearpants@users.noreply.github.com>
2025-03-10 14:21:30 -0700

    correct sign typo in the paper

--- script/singledefect_test.m
+++ script/singledefect_test.m
@@ -4,16 +4,16 @@ t = -0.2;           % hopping parameter
 E0 = 0;             % on-site energy
 n = 300;            % number of grid points for numerical integration
 epsilon = 1e-3;     % small imaginary part for numerical stability
-gridSize = 256;      % number of sampling points along one dimension
-omega_values = linspace(-0.5, 0.5, 41);  % energy levels
+gridSize = 51;      % number of sampling points along one dimension
+omega_values = linspace(-1, 1, 11);  % energy levels
 
 %% 2. Single defect setup
 defect_location = [0, 0];    % Single defect at origin
-defect_energy = 1.0;         % Defect energy E1
+defect_energy = 0.002;         % Defect energy E1
 
 %% 3. Create spatial grid for LDOS visualization
-x_range = linspace(-25*a, 25*a, gridSize);
-y_range = linspace(-25*a, 25*a, gridSize);
+x_range = linspace(-10*a, 10*a, gridSize);
+y_range = linspace(-10*a, 10*a, gridSize);
 [X, Y] = meshgrid(x_range, y_range);
 X_physical = cat(3, X, Y);
 %% 4. Initialize arrays for results
@@ -40,7 +40,7 @@ for i = 1:length(omega_values)
     delta_rho_1(:,:,i) = -1/pi * imag(G0_xd * T_single(i)* G0_dx);
 end
 
-% 5.2 G0_xd * T_single(i)* G0_dx'
+%% 5.2 G0_xd * T_single(i)* G0_dx'
 for i = 1:length(omega_values)
     % Compute G0(xd,xd) - Green's function at defect location
     G0_dd = computeBLGF(defect_location, defect_location, omega_values(i), a, t, E0, n, epsilon);
@@ -57,7 +57,7 @@ for i = 1:length(omega_values)
 end
 
 % 5.3 G0_xd * T_single(i)* conj(G0_dx')
-for i = 1:length(omega_values)
+for i = 1:length(omega_values) 
     % Compute G0(xd,xd) - Green's function at defect location
     G0_dd = computeBLGF(defect_location, defect_location, omega_values(i), a, t, E0, n, epsilon);
     
@@ -72,7 +72,7 @@ for i = 1:length(omega_values)
     delta_rho_3(:,:,i) = -1/pi * imag(G0_xd * T_single(i)* conj(G0_dx'));
 end
 
-% 5.4 G0_xd.*G0_xd * T_single(i)
+%% 5.4 G0_xd.*G0_xd * T_single(i)
 for i = 1:length(omega_values)
     % Compute G0(xd,xd) - Green's function at defect location
     G0_dd = computeBLGF(defect_location, defect_location, omega_values(i), a, t, E0, n, epsilon);
@@ -88,7 +88,7 @@ for i = 1:length(omega_values)
     delta_rho_4(:,:,i) = -1/pi * imag(G0_xd.*G0_xd * T_single(i));
 end
 
-% 5.5 G0_xd.*G0_xd' * T_single(i)
+%% 5.5 G0_xd.*G0_xd' * T_single(i)
 for i = 1:length(omega_values)
     % Compute G0(xd,xd) - Green's function at defect location
     G0_dd = computeBLGF(defect_location, defect_location, omega_values(i), a, t, E0, n, epsilon);
